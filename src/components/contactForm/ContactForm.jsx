f5a9ba890e55f0b1092806029fc78ff6d2d15797
Roman <romeenice@gmail.com>
2023-03-23 22:53:55 +0200

    Update ContactForm.jsx

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,68 +1,126 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
-import PropTypes from 'prop-types';
 
-export default class ContactForm extends React.Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function ContactForm() {
+  const [name, setName] = React.useState('');
+  const [number, setNumber] = React.useState('');
 
-  handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
-  handleSubmit = (e) => {
+
+  const handleSubmit = e => {
     e.preventDefault();
 
     const contact = {
       id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
+      name: name,
+      number: number,
     };
 
     this.props.onAddContact(contact);
 
-    this.setState({
-      name: '',
-      number: '',
-    });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    return (
-      <div className="container">
-        <h3 className="title">Name</h3>
-        <input
-          className="input_name"
-          type="text"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters and apostrophes, and must start and end with a letter."
-          required
-        />
-        <h3 className="title">Number</h3>
-        <input
-          className="input_number"
-          type="tel"
-          name="number"
-          value={this.state.number}
-          onChange={this.handleChange}
-          pattern="^[0-9]{3}-[0-9]{2}-[0-9]{2}$"
-          title="Number must be in format 000-00-00"
-          required
-        />
-        <button className="button" type="submit" onClick={this.handleSubmit}>
-          Add contact
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <h3 className="title">Name</h3>
+      <input
+        className="input_name"
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters and apostrophes, and must start and end with a letter."
+        required
+      />
+      <h3 className="title">Number</h3>
+      <input
+        className="input_number"
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        pattern="^[0-9]{3}-[0-9]{2}-[0-9]{2}$"
+        title="Number must be in format 000-00-00"
+        required
+      />
+      <button className="button" type="submit" onClick={handleSubmit}>
+        Add contact
+      </button>
+    </div>
+  );
 }
 
-ContactForm.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onAddContact: PropTypes.func.isRequired,
-};
+// export default class ContactForm extends React.Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = (e) => {
+//     const { name, value } = e.target;
+//     this.setState({ [name]: value });
+//   };
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     const contact = {
+//       id: nanoid(),
+//       name: this.state.name,
+//       number: this.state.number,
+//     };
+
+//     this.props.onAddContact(contact);
+
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <h3 className="title">Name</h3>
+//         <input
+//           className="input_name"
+//           type="text"
+//           name="name"
+//           value={this.state.name}
+//           onChange={this.handleChange}
+//           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//           title="Name may contain only letters and apostrophes, and must start and end with a letter."
+//           required
+//         />
+//         <h3 className="title">Number</h3>
+//         <input
+//           className="input_number"
+//           type="tel"
+//           name="number"
+//           value={this.state.number}
+//           onChange={this.handleChange}
+//           pattern="^[0-9]{3}-[0-9]{2}-[0-9]{2}$"
+//           title="Number must be in format 000-00-00"
+//           required
+//         />
+//         <button className="button" type="submit" onClick={this.handleSubmit}>
+//           Add contact
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// ContactForm.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onAddContact: PropTypes.func.isRequired,
+// };
